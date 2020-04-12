72d06daf32b4a99fefdef5be52c96e3c80b144d9
Brian Foster <brriann@gmail.com>
2020-04-12 15:49:34 -0600

    read file to 2d array and validator stubs and readme edits

--- README.md
+++ README.md
@@ -2,10 +2,17 @@
 A take-home coding assignment; console app that validates Sudoku puzzles
 
 To all who evaluate this solution:
-I'm making an attempt to timebox this effort, and get a working solution.
+In the spirit of timeboxing this to <4 hours, I'll prioritize a working solution over 100% ideal design.
+
 Along the way, I'll make notes of design decisions that I'd prefer to clean up.
+
+If any code that appears is a "dealbreaker", please let me know, and I can provide a refactor commit.
 
 1. ReadFileTo2DArray()
 Initially, I'll return null if file doesn't parse to a 9x9 grid of numbers where 1<=x<=9.
-Ideally, I'd throw a meaningful exception.
+Ideally, I'd throw a meaningful exception.  I avoid null returns in production code.
+
+Initially, this method will have multiple return locations.
+Ideally, I'd have a single return location, instead of falling out with null returns in many locations
+	as I catch errors.
 
--- SudokuGrader/Program.cs
+++ SudokuGrader/Program.cs
@@ -6,7 +6,82 @@ namespace SudokuGrader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string FILEPATH;
+            int[][] sudokuBoard;
+            bool validSudokuSolution;
+
+            // use a file path if one was passed in.  else, use hardcoded file path.
+            if (args.Length == 1)
+            {
+                Console.WriteLine("reading filePath from CLI args");
+                FILEPATH = args[0];
+            }
+            else
+            {
+                Console.WriteLine(@"usage: ./SudokuGrader C:\path\to\sudoku\txt\file.txt");
+                FILEPATH = @"C:\Development\SudokuGrader\TestFiles\sudoku.txt";
+            }
+            
+            sudokuBoard = ValidateFileAndReadTo2DArray(FILEPATH);
+
+            if (sudokuBoard == null)
+            {
+                Console.WriteLine("Invalid file format. Expected a 9-line .txt file with 9 digits.");
+                return;
+            }
+
+
+        }
+
+        public static bool ValidateRows(int[][] sudokuBoard)
+        {
+            return true;
+        }
+
+        public static bool ValidateColumns(int[][] sudokuBoard)
+        {
+            return true;
+        }
+
+        public static bool ValidateSquares(int[][] sudokuBoard)
+        {
+            return true;
+        }
+
+        public static int[][] ValidateFileAndReadTo2DArray(string filePath)
+        {
+            string[] fileLines = System.IO.File.ReadAllLines(filePath);
+
+            if (fileLines.Length != 9)
+            {
+                return null;
+            }
+
+            int[][] fileAs2DArray = new int[9][];
+
+            for (int i = 0; i < 9; i++) // fileLines.Length is confirmed to be 9 above.
+            {
+                string[] row = fileLines[i].Split(' ');
+
+                if (row.Length != 9)
+                {
+                    return null;
+                }
+
+                int[] intRow = new int[9];
+                for (int j = 0; j < 9; j++) // fileLines.Length is confirmed to be 9 above.
+                {
+                    int cellValue;
+                    if (!int.TryParse(row[j], out cellValue) || cellValue < 1 || cellValue > 9)
+                    {
+                        return null;
+                    }
+
+                    intRow[j] = cellValue;
+                }
+                fileAs2DArray[i] = intRow;
+            }
+            return fileAs2DArray;
         }
     }
 }
